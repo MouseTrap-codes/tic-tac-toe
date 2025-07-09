8a835bb23293b7ff4e9aaa7ef83eb9b4be1993d7
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-07-09 00:31:01 -0500

    Complete logic for GameController (except for draw detection)

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ function Gameboard() {
     const getBoard = () => board;
 
     // logic for placing a token (X or O)
-    const placeToken(row, column, player) {
+    const placeToken = (row, column, player) => {
         if (board[row][column].getValue() === "") {
             board[row][column].addToken(player);
         } else {
@@ -29,6 +29,9 @@ function Gameboard() {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.log(boardWithCellValues);
     }
+
+    // public api
+    return {getBoard, placeToken, printBoard};
 }
 
 function Cell() {
@@ -36,7 +39,7 @@ function Cell() {
 
     // accept a player's token and set it to X or O
     const addToken = (player) => {
-        value - player;
+        value = player;
     }
 
     // getter to retrieve value of cell
@@ -47,3 +50,104 @@ function Cell() {
         getValue
     };
 }
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: "X"
+        },
+        {
+            name: playerTwoName,
+            token: "O"
+        }
+    ]
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    }
+
+    const winConditionMet = function() {
+            let won = false;
+
+            // 1) current player has filled an entire row
+            const filledRow = board.getBoard().some((row) => {
+                const [a, b, c] = row.map(cell => cell.getValue());
+                return a !== "" && a === b && a === c && a === getActivePlayer().token;
+            })
+
+            // 2) if current player has filled an entire column
+            const filledColumn = function() {
+                let columnFilled = false;
+                const boardState = board.getBoard();
+                
+                // loop through each column
+                for (let col = 0; col < boardState[0].length; col++) {
+                    let firstRowVal = boardState[0][col].getValue();
+                    let secondRowVal = boardState[1][col].getValue();
+                    let thirdRowVal =boardState[2][col].getValue();
+                    if (firstRowVal === getActivePlayer().token &&
+                        firstRowVal === secondRowVal &&
+                        secondRowVal === thirdRowVal) {
+                            columnFilled = true;
+                    }
+                }
+
+                return columnFilled;
+            }
+
+            // 3) if current player has filled a diagonal
+            const filledDiagonal = function() {
+                const boardState = board.getBoard();
+                return (
+                    boardState[0][0].getValue() === boardState[1][1].getValue() &&  boardState[1][1].getValue() === boardState[2][2].getValue() && boardState[2][2].getValue() === getActivePlayer().token 
+                    ||
+                    boardState[2][0].getValue() === boardState[1][1].getValue() &&  boardState[1][1].getValue() === boardState[0][2].getValue() && boardState[0][2].getValue() === getActivePlayer().token 
+                );
+            }
+
+            won = filledRow || filledColumn() || filledDiagonal();
+
+            return won;
+
+        }
+
+    const playRound = (row, column) => {
+        // place a token for current player
+        console.log(`Placing ${getActivePlayer().name}'s ${getActivePlayer().token} into row ${row}, column ${column}`);
+        board.placeToken(row, column, getActivePlayer().token);
+
+         // win conditions here!!!
+        if (winConditionMet()) {
+            console.log(`${getActivePlayer().name} has won!!!`);
+            return;
+        }
+        
+        // switch player turn
+        switchPlayerTurn();
+        printNewRound();
+    }
+
+   
+
+    // initial play game message
+    printNewRound();
+
+    return {
+        playRound, 
+        getActivePlayer
+    };
+}
+
+const game = GameController();
